ef4081a5e7bb94f69e78148c248309c884d7610c
vvfsendwhatsapp <vvfsendwhatsapp@gmail.com>
2026-07-30 17:02:18 +0200

    1

--- script.js
+++ script.js
@@ -292,9 +292,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         container.innerHTML = `
             <div class="riepilogo-box">
-                <h3>Sala Operativa - Comando VVF ${comando.Comando}</h3>
+                <h3>Comando VVF ${comando.Comando}</h3>
 
-                <h4 class="sezione-toggle" data-target="sezione-comando">Comando</h4>
+                <h4 class="sezione-toggle" data-target="sezione-comando">Sala Operativa Comando</h4>
                 <table id="sezione-comando" class="riepilogo-tabella sezione-contenuto">
                     <tbody>
                         <tr><th>Indirizzo</th><td>${creaLinkMaps(comando["Indirizzo Completo"], comando["Coordinate"])}</td></tr>
@@ -310,7 +310,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     </tbody>
                 </table>
 
-                <h4 class="sezione-toggle" data-target="sezione-direzione">Direzione VVF</h4>
+                <h4 class="sezione-toggle" data-target="sezione-direzione">Direzione VVF ${comando.Comando}">${comando["Direzione VVF"] || "-"}</h4>
                 <table id="sezione-direzione" class="riepilogo-tabella sezione-contenuto">
                     <tbody>
                         <tr><th>Direzione</th><td class="cliccabile" data-direzione-di="${comando.Comando}">${comando["Direzione VVF"] || "-"}</td></tr>
