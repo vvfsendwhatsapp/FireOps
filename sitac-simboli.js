a9a5d360fcf0b7ce0962ddd47581999556c1de5a
vvfsendwhatsapp <vvfsendwhatsapp@gmail.com>
2026-09-07 17:04:22 +0200

    S

--- sitac-simboli.js
+++ sitac-simboli.js
@@ -1025,7 +1025,7 @@ aggL('linea_sicurezza','azioni','sgControfuoco','Creazione linea di sicurezza','
    punta e deve pareggiarlo, `fuori` porta la base sul bordo. Cambiandone
    uno vanno rivisti gli altri. */
 
-agg('accensione_linee_p','azioni','sgControfuoco','Accensione per linee (punto)',
+agg('accensione_linee_p','azioni','sgControfuoco','Accensione per linee (punto) DA SISTEMARE',
   'Line firing (point)', accensioneDiretta(),
   {r:1, r0:0, s:1, senzaDisco:1, lungo:1, senzAsta:1,
    asta:{color:'#ffffff', weight:16, guaina:22, bordo:C.rosso,
