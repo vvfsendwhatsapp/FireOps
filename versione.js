353937053e35e3f14762182158c552c1e3cf747d
vvfsendwhatsapp <vvfsendwhatsapp@gmail.com>
2026-09-19 15:43:28 +0200

    as

--- versione.js
+++ versione.js
@@ -8,7 +8,7 @@
 
 
 
-const FIREOPS_VERSIONE = "202609191521";
+const FIREOPS_VERSIONE = "202609191522";
 window.FIREOPS_VERSIONE = FIREOPS_VERSIONE;
 
 /* Il foglio di stile passa da qui: un solo numero da aggiornare, e viene
